48220f15dba1fe8a831b4533377cd591e99c2638
eshandro <erik843@gmail.com>
2014-07-25 14:21:20 -0600

    Progress made

--- app.js
+++ app.js
@@ -10,24 +10,50 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
 app.get('/', function(req, res) {
-	res.render('index');
+	if (submissions.length < 8) {
+		res.render('index');
+	}
+	else {
+		res.render('contest-full');
+	}
 });
 
 app.post('/formsubmit', function(req, res) {
 	var name = req.body.name;
 	var url = req.body.url;
+	// Gets needed part of url to embed the youtube video on view video page
+	var index = url.indexOf('=');
+	url = url.slice(index+1);
+
 	var title = req.body.title;
 	var descr = req.body.description;
 	var newSubmission = new Submission(name, url, title, descr);
 	if(submissions.length < 8) {
 		submissions.push(newSubmission);
+		res.redirect('/entries')
 	}
 	else {
-		res.send('/', '<p>Max number of submissions reached</p>')
+		res.redirect('/contest-full')
 	}
-	res.render('current-submissions')
-
 })
+
+app.get('/entries', function(req,res) {
+	res.render('current-submissions', {
+		currentSubmissions: submissions
+	})	
+})
+
+app.get('/contest-full', function(req, res) {
+	res.render('contest-full');
+})
+
+// Preloading videos for testing
+submissions.push(new Submission('Bob', 'AsrsrlPrWmM', 'Is the the best Tiger Woods Video Ever', 'Tiger Woods video'))
+submissions.push(new Submission('George', 'jCIP_WzhBg8', 'Funny professional golfer bloopers', 'Pro golfer mistakes'))
+submissions.push(new Submission('Susie', 'z70XlpjnEb8', 'Charl Schwartzel shot off the road', 'Amazing golf shot'))
+submissions.push(new Submission('Sara', 'blonNcv1yas', 'Tiger Woods - Pro Am swing footage', 'Tiger Woods swing footage'))
+submissions.push(new Submission('Timmy', 'EIcKQkzqgok', 'Top 10 PGA Tour 2013', 'Best moments PGA 2013'))
+submissions.push(new Submission('Dave', 'DyrY97vaDis', 'Rory McIlroy - Golf swing slow mo', 'Rory McIlroy slow-mo golf swing'))
 
 var server = app.listen(3417, function() {
 	console.log('Express server listening on port ' + server.address().port);
