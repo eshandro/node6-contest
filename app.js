fea7a1c9c275a2c83ae8189b0a8b726179768584
Erik Shandrowski <erik843@gmail.com>
2014-07-25 11:35:00 -0600

    Videos added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var submissions = require('./models/submissions.js');
-var addSubmission = require('./controllers/addSubmissions.js')
+var Submission = require('./controllers/addSubmissions.js')
 
 var app = express();
 app.set('view engine', 'jade');
@@ -14,23 +14,21 @@ app.get('/', function(req, res) {
 });
 
 app.post('/formsubmit', function(req, res) {
-	console.log(addSubmission)
 	var name = req.body.name;
 	var url = req.body.url;
 	var title = req.body.title;
 	var descr = req.body.description;
-	var newSubmission = new addSubmission(name, url, title, descr);
-	console.log(newSubmission)
-	submissions.push(newSubmission);
-	console.log(submissions)
-	res.send(JSON.stringify(newSubmission));
+	var newSubmission = new Submission(name, url, title, descr);
+	if(submissions.length < 8) {
+		submissions.push(newSubmission);
+	}
+	else {
+		res.send('/', '<p>Max number of submissions reached</p>')
+	}
+	res.render('current-submissions')
+
 })
 
-/*var test = function() {
-	console.log(addSubmission);
-}
-test();
-*/
 var server = app.listen(3417, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
